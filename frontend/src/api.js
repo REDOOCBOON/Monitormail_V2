910dc5fce254606321882fdf8daa98d6f4df620b
Purrfect Developer <test@example.com>
2026-03-16 01:47:52 +0530

    connect backend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:5000'; // Change to your deployed backend URL in production
+const API_URL = "https://monitormail-api.onrender.com"; // Change to your deployed backend URL in production
 
 const request = async (endpoint, options) => {
     const token = localStorage.getItem('token');
