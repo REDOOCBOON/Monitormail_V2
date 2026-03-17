86ef5f6a9e875ffa5377ac67e2df88e02cca3d3f
Purrfect Developer <test@example.com>
2026-03-18 01:34:23 +0530

    fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://127.0.0.1:5000"; // Local backend for development
+// const API_URL = "http://127.0.0.1:5000"; // Local backend for development
+const API_URL = process.env.REACT_APP_API_URL || "https://monitormail-api.onrender.com";
+
 
 const request = async (endpoint, options) => {
     const token = localStorage.getItem('token');
